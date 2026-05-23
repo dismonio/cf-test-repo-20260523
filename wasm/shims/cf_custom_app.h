89a0c518d1ba9973b741088703d40970a606ca40
Sam Steele <34994139+dismonio@users.noreply.github.com>
2026-05-23 17:01:06 -0400

    Initialize from template: wasm/shims/cf_custom_app.h

---
+++ wasm/shims/cf_custom_app.h
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2023-2026 Dismo Industries LLC
+
+// Auto-included for custom app WASM builds.
+// Provides all common headers so generated code compiles even if
+// the app builder forgets to #include something.
+
+#ifndef CF_CUSTOM_APP_H
+#define CF_CUSTOM_APP_H
+
+#include "App.h"
+#include <Arduino.h>
+#include "HAL.h"
+#include "globals.h"
+#include "DisplayProxy.h"
+#include "ButtonManager.h"
+#include "AudioManager.h"
+#include "RGBController.h"
+#include "MenuManager.h"
+
+#endif

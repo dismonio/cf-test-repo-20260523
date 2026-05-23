c184c681994935dbf3661b452bc8569fdb0f2a99
Sam Steele <34994139+dismonio@users.noreply.github.com>
2026-05-23 17:01:08 -0400

    Initialize from template: wasm/shims/esp_system.h

---
+++ wasm/shims/esp_system.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2023-2026 Dismo Industries LLC
+
+#ifndef WASM_ESP_SYSTEM_H
+#define WASM_ESP_SYSTEM_H
+
+inline void esp_restart() {}
+
+#endif

099ae9a18a479d7f6059121c038f1a182b590956
Sam Steele <34994139+dismonio@users.noreply.github.com>
2026-05-23 17:01:09 -0400

    Initialize from template: wasm/shims/esp_task_wdt.h

---
+++ wasm/shims/esp_task_wdt.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (c) 2023-2026 Dismo Industries LLC
+
+#ifndef WASM_ESP_TASK_WDT_H
+#define WASM_ESP_TASK_WDT_H
+
+inline void esp_task_wdt_reset() {}
+
+#endif
